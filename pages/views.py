694adc60b3c58efb623bf055c8e692fe4278d0ea
Philip Schleihauf <uniphil@gmail.com>
2013-06-18 23:35:04 -0400

    security shmecurity

--- pages/views.py
+++ pages/views.py
@@ -72,14 +72,14 @@ def crossdomain(request):
     <!-- Read this: www.adobe.com/devnet/articles/crossdomain_policy_file_spec.html -->
 
     <!-- Most restrictive policy: -->
+    <!--
     <site-control permitted-cross-domain-policies="none"/>
+    -->
 
     <!-- Least restrictive policy: -->
-    <!--
     <site-control permitted-cross-domain-policies="all"/>
     <allow-access-from domain="*" to-ports="*" secure="false"/>
     <allow-http-request-headers-from domain="*" headers="*" secure="false"/>
-    -->
 </cross-domain-policy>
 """
     return HttpResponse(policy, content_type='application/xml')
